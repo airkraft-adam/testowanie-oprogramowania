2db61cee6fd89637792ed67a016c8f0dd847ea52
airkraft-adam <55787074+airkraft-adam@users.noreply.github.com>
2021-01-03 18:55:11 +0100

    Add files via upload

---
+++ Projekt_zaliczenie/Object Repository/PageLogin/button_next.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Przycisk dalej</description>
+   <name>button_next</name>
+   <tag></tag>
+   <elementGuidId>179c1c9d-760d-47a1-b3de-47be79aad951</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#login_next</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Projekt_zaliczenie/Object Repository/PageLogin/input.login.id.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Identyfikator użytkownika</description>
+   <name>input.login.id</name>
+   <tag></tag>
+   <elementGuidId>428fd452-4fe4-4ab9-a4fd-76e8f4dd88bc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#login_id</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
